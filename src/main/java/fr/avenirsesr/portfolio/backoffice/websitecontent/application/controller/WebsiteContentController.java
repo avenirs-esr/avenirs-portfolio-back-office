f14bb74f16c544f4c9362f8d1f1e111c0d1ec457
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-10-13 16:31:38 +0200

    fix: comments

--- src/main/java/fr/avenirsesr/portfolio/backoffice/websitecontent/application/controller/WebsiteContentController.java
+++ src/main/java/fr/avenirsesr/portfolio/backoffice/websitecontent/application/controller/WebsiteContentController.java
@@ -38,7 +38,7 @@ public class WebsiteContentController {
   @GetMapping(path = "/setup/build-life-project")
   public ResponseEntity<Map<ELanguage, BuildLifeProjectConfigDTO>>
       getBuildLifeProjectConfigWithAllTranslations() {
-    log.debug("Received request to get build life project config");
+    log.debug("Received request to get build life project config with all translations");
 
     Map<ELanguage, BuildLifeProjectConfiguration> config =
         websiteContentConfigurationService.getLifeProjectConfigurationWithAllTranslations();
@@ -53,7 +53,7 @@ public class WebsiteContentController {
 
   @GetMapping(path = "/build-life-project")
   public ResponseEntity<BuildLifeProjectConfigDTO> getBuildLifeProjectConfig() {
-    log.debug("Received request from to get build life project config");
+    log.debug("Received request to get build life project config");
 
     BuildLifeProjectConfiguration config =
         websiteContentConfigurationService.getLifeProjectConfiguration();
