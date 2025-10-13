172b80628add3d7bd278f649389b959846649cfc
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-10-13 15:48:52 +0200

    fix: injection of Principal in WebsiteContentController

--- src/main/java/fr/avenirsesr/portfolio/backoffice/websitecontent/application/controller/WebsiteContentController.java
+++ src/main/java/fr/avenirsesr/portfolio/backoffice/websitecontent/application/controller/WebsiteContentController.java
@@ -52,8 +52,8 @@ public class WebsiteContentController {
   }
 
   @GetMapping(path = "/build-life-project")
-  public ResponseEntity<BuildLifeProjectConfigDTO> getBuildLifeProjectConfig(Principal principal) {
-    log.debug("Received request from {} to get build life project config", principal.getName());
+  public ResponseEntity<BuildLifeProjectConfigDTO> getBuildLifeProjectConfig() {
+    log.debug("Received request from to get build life project config");
 
     BuildLifeProjectConfiguration config =
         websiteContentConfigurationService.getLifeProjectConfiguration();
