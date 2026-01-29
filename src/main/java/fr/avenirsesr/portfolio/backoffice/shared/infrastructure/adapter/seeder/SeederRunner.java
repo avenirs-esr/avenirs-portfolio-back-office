2a2ee60093f2b89c48022697beec59096ac7aeed
Nathan <nathan.pignon79@gmail.com>
2026-01-29 13:53:21 +0100

    fix(1021): seeding state implementation for health indicator

--- src/main/java/fr/avenirsesr/portfolio/backoffice/shared/infrastructure/adapter/seeder/SeederRunner.java
+++ src/main/java/fr/avenirsesr/portfolio/backoffice/shared/infrastructure/adapter/seeder/SeederRunner.java
@@ -6,6 +6,7 @@ import fr.avenirsesr.portfolio.backoffice.shared.domain.model.EConfigurationScop
 import fr.avenirsesr.portfolio.backoffice.shared.domain.port.output.repository.ConfigurationRepository;
 import fr.avenirsesr.portfolio.backoffice.trace.infrastructure.seeder.TraceConfigSeeder;
 import fr.avenirsesr.portfolio.backoffice.websitecontent.infrastructure.seeder.WebsiteContentConfigurationSeeder;
+import fr.avenirsesr.portfolio.common.seeder.infrastructure.configuration.SeedingState;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,6 +22,7 @@ public class SeederRunner implements CommandLineRunner {
   private final WebsiteContentConfigurationSeeder websiteContentConfigurationSeeder;
   private final InstitutionConfigSeeder institutionConfigSeeder;
   private final ConfigurationRepository configurationRepository;
+  private final SeedingState seedingState;
 
   @Value("${seeder.enabled:false}")
   private boolean seedEnabled;
@@ -30,31 +32,42 @@ public class SeederRunner implements CommandLineRunner {
       TraceConfigSeeder traceConfigSeeder,
       WebsiteContentConfigurationSeeder websiteContentConfigurationSeeder,
       InstitutionConfigSeeder institutionConfigSeeder,
-      ConfigurationRepository configurationRepository) {
+      ConfigurationRepository configurationRepository,
+      SeedingState seedingState) {
     this.additionalSkillConfigSeeder = additionalSkillConfigSeeder;
     this.traceConfigSeeder = traceConfigSeeder;
     this.websiteContentConfigurationSeeder = websiteContentConfigurationSeeder;
     this.institutionConfigSeeder = institutionConfigSeeder;
     this.configurationRepository = configurationRepository;
+    this.seedingState = seedingState;
   }
 
   @Transactional
   @Override
   public void run(String... args) {
-    long websiteConfCount =
-        configurationRepository.inScope(EConfigurationScope.WEBSITE_CONTENT).size();
+    try {
+      long websiteConfCount =
+          configurationRepository.inScope(EConfigurationScope.WEBSITE_CONTENT).size();
 
-    if (seedEnabled && websiteConfCount == 0) {
-      log.info("Seeding enabled and starting...");
+      if (seedEnabled && websiteConfCount == 0) {
+        log.info("Seeding enabled and starting...");
 
-      additionalSkillConfigSeeder.seed();
-      traceConfigSeeder.seed();
-      websiteContentConfigurationSeeder.seed();
-      institutionConfigSeeder.seed(List.of());
+        additionalSkillConfigSeeder.seed();
+        traceConfigSeeder.seed();
+        websiteContentConfigurationSeeder.seed();
+        institutionConfigSeeder.seed(List.of());
 
-      log.info("✔ Seeding successfully finished");
-    } else
-      log.info(
-          "{} website configurations found. Seeder is disabled: seeding skipped", websiteConfCount);
+        log.info("✔ Seeding successfully finished");
+      } else {
+        log.info(
+            "{} website configurations found. Seeder is disabled: seeding skipped",
+            websiteConfCount);
+      }
+      seedingState.markCompleted();
+    } catch (Exception e) {
+      seedingState.markFailed(e);
+      log.error("✘ Seeding failed", e);
+      throw e;
+    }
   }
 }
